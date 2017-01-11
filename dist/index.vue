e0ae9231e442957fcf2b2246a24ccee43b19c74e
2046 <g@ggg.gd>
2017-01-11 11:06:07 +0800

    fixed bug

--- dist/index.vue
+++ dist/index.vue
@@ -1,5 +1,4 @@
 <style src="./style.css"></style>
-<template src="./template.html"></template>
 <script>
     import Vue from 'vue'
     import plugin from './plugin'
@@ -17,7 +16,32 @@
                 confirmAccess: false
             }
         },
+        render(h) {
+            return (
+                <transition name="ui-confirm-fade">
+                    <div class="ui-confirm" v-show={this.visible}>
+                        <transition name="ui-confirm-zoomOut">
+                            <div class="ui-confirm__container" v-show={this.visible}>
+                                <p class="ui-confirm__title">{this.$slots.title}</p>
+                                <p class="ui-confirm__info">{this.$slots.text}</p>
+                                <div class="ui-confirm__button">
+                                    <div class={{"ui-confirm__button--ok": true, "ui-confirm-button--access": this.confirmAccess}} onClick={this.emit.bind(this, true)} onTouchstart={this.confirmHandle.bind(this, 0)} touchend={this.confirmHandle.bind(this, 200)}>
+                                        {this.$slots.confirmButton || '确定'}
+                                    </div>
+                                    <div class={{"ui-confirm__button--cancel": true, "ui-confirm-button--access": this.cancelAccess}} onClick={this.emit.bind(this, false)} onTouchstart={this.confirmHandle.bind(this, 0)} touchend={this.confirmHandle.bind(this, 200)}>
+                                        {this.$slots.cancelButton || '取消'}
+                                    </div>
+                                </div>
+                            </div>
+                        </transition>
+                    </div>
+                </transition>
+            )
+        },
         methods: {
+            emit(bool) {
+                this.$emit('hide', bool)
+            },
             confirmHandle(time) {
                 setTimeout(() => {
                     this.confirmAccess = !this.confirmAccess
@@ -31,7 +55,7 @@
         }
     }
 
-    Vue.use(plugin(Component))
+    Vue.use(plugin, Component)
     
     export default Component
 </script>
--- src/plugin.js
+++ src/plugin.js
@@ -1,74 +1,74 @@
-export default function (Confirm) {
-    return function (Vue) {
-        let Component, component, container, props
+function plugin(Vue, Confirm) {
+    let Component, component, container, props
 
-        props = {
-            text: '',
-            title: '',
-            hide() {},
-            visible: false,
-            cancelButton: '取消',
-            confirmButton: '确定',
-        }
-
-        Component = Vue.extend({
-            props: {
-                options: {
-                    type: Object,
-                    default () {
-                        return props
-                    }
-                }
-            },
-            render(createElement) {
-                let children = [
-                    createElement('p', {
-                        slot: 'cancelButton'
-                    }, this.options.cancelButton),
-                    createElement('p', {
-                        slot: 'confirmButton'
-                    }, this.options.confirmButton)
-                ]
-
-                if (this.options.title) {
-                    children.push(createElement('p', {
-                        slot: 'title'
-                    }, this.options.title))
-                }
-
-                if (this.options.text) {
-                    children.push(createElement('p', {
-                        slot: 'text'
-                    }, this.options.text))
-                }
-
-                return createElement(Confirm, {
-                    props: {
-                        visible: this.options.visible
-                    },
-                    on: {
-                        hide: function (val) {
-                            this.options.visible = false
-                            this.options.hide(val)
-                        }.bind(this)
-                    }
-                }, children)
-            }
-        })
-
-        container = document.createElement('div')
-        document.getElementsByTagName('body')[0].appendChild(container)
-        component = new Component().$mount(container)
-
-        function method(options) {
-            component.options = {
-                ...props,
-                ...options,
-                visible: true
-            }
-        }
-
-        Vue.confirm = method
-        Vue.prototype.$confirm = method
+    props = {
+        text: '',
+        title: '',
+        hide() {},
+        visible: false,
+        cancelButton: '取消',
+        confirmButton: '确定',
     }
+
+    Component = Vue.extend({
+        props: {
+            options: {
+                type: Object,
+                default () {
+                    return props
+                }
+            }
+        },
+        render(createElement) {
+            let children = [
+                createElement('p', {
+                    slot: 'cancelButton'
+                }, this.options.cancelButton),
+                createElement('p', {
+                    slot: 'confirmButton'
+                }, this.options.confirmButton)
+            ]
+
+            if (this.options.title) {
+                children.push(createElement('p', {
+                    slot: 'title'
+                }, this.options.title))
+            }
+
+            if (this.options.text) {
+                children.push(createElement('p', {
+                    slot: 'text'
+                }, this.options.text))
+            }
+
+            return createElement(Confirm, {
+                props: {
+                    visible: this.options.visible
+                },
+                on: {
+                    hide: function (val) {
+                        this.options.visible = false
+                        this.options.hide(val)
+                    }.bind(this)
+                }
+            }, children)
+        }
+    })
+
+    container = document.createElement('div')
+    document.getElementsByTagName('body')[0].appendChild(container)
+    component = new Component().$mount(container)
+
+    function method(options) {
+        component.options = {
+            ...props,
+            ...options,
+            visible: true
+        }
+    }
+
+    Vue.confirm = method
+    Vue.prototype.$confirm = method
 }
+
+module.exports = plugin
