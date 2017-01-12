8d6ac5465d3bf3aa77442bfc225804563d910f07
2046 <g@ggg.gd>
2017-01-12 16:56:16 +0800

    fixed bug

--- dist/index.vue
+++ dist/index.vue
@@ -25,10 +25,10 @@
                                 <p class="ui-confirm__title">{this.$slots.title}</p>
                                 <p class="ui-confirm__info">{this.$slots.text}</p>
                                 <div class="ui-confirm__button">
-                                    <div class={{"ui-confirm__button--ok": true, "ui-confirm-button--access": this.confirmAccess}} onClick={this.emit.bind(this, true)} onTouchstart={this.confirmHandle.bind(this, 0)} touchend={this.confirmHandle.bind(this, 200)}>
+                                    <div class={{"ui-confirm__button--ok": true, "ui-confirm-button--access": this.confirmAccess}} onClick={this.emit.bind(this, true)} onTouchstart={this.confirmHandle.bind(this, 0)} onTouchend={this.confirmHandle.bind(this, 200)}>
                                         {this.$slots.confirmButton || '确定'}
                                     </div>
-                                    <div class={{"ui-confirm__button--cancel": true, "ui-confirm-button--access": this.cancelAccess}} onClick={this.emit.bind(this, false)} onTouchstart={this.cancelHandle.bind(this, 0)} touchend={this.cancelHandle.bind(this, 200)}>
+                                    <div class={{"ui-confirm__button--cancel": true, "ui-confirm-button--access": this.cancelAccess}} onClick={this.emit.bind(this, false)} onTouchstart={this.cancelHandle.bind(this, 0)} onTouchend={this.cancelHandle.bind(this, 200)}>
                                         {this.$slots.cancelButton || '取消'}
                                     </div>
                                 </div>
